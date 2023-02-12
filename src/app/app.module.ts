c0294df46ecc7d273d87897212fb3e6bfa2ab0fa
Emanuelle30 <emanuelle.cavalcante4@gmail.com>
2023-02-12 20:49:56 -0300

    criando component de forma manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuManualComponent } from './meu-componente-manual/meu-manual.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent
+    MeuPrimeiroComponent,
+    MeuManualComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meu-componente-manual/meu-manual.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-manual-component',
+    template: `
+    <p> Meu primeiro component manual com Angular2!`
+})
+export class MeuManualComponent {}
