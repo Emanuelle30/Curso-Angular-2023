c0d14d6e6a72c491373304d6b62b86607b26fbaa
Emanuelle30 <emanuelle.cavalcante4@gmail.com>
2023-02-12 21:35:12 -0300

    testando TS e JS

---
+++ src/app/hello-tyspescript/main.ts
@@ -0,0 +1,23 @@
+var minhaVar = "minha variavel";
+
+
+function minhaFunc(x: any, y: any){
+    return x + y;
+}
+
+let num = 2;
+
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2);
+
+class Matematica {
+    soma(x: any, y: any){
+        return x + y;
+    }
+}
